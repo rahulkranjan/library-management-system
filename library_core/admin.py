a6454fee1abb1a575d2dd136bf05f4521d37c45e
Rahul Kumar Ranjan <kjrahul21@gmail.com>
2024-12-16 16:03:00 +0530

    Update admin.py

--- library_core/admin.py
+++ library_core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from library_core.models import *
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BorrowRecord)
